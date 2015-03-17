102a4daf9b0262d6cd64c5d87fd656f387ab16f0
Edgar <edgarapazac@gmail.com>
2015-03-17 11:17:42 -0500

    Enviado todo los cambios en django

---
+++ mysite/polls/migrations/0002_alumno.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Alumno',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('nombres', models.CharField(max_length=100)),
+                ('apellidos', models.CharField(max_length=100)),
+                ('edad', models.IntegerField()),
+                ('num_dni', models.CharField(max_length=8)),
+            ],
+        ),
+    ]
